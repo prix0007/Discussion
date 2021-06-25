eae4426fbb45c72eeeb01a5d8636c6fad6994363
Prince Anuragi <princanurag07@gmail.com>
2021-06-25 11:20:52 +0530

    Added Comments on Individual Topics

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -14,6 +14,12 @@ defmodule DiscussWeb.TopicController do
     render conn, "index.html", topics: topics
   end
 
+  def show(conn, %{"id" => topic_id}) do
+    topic = Topic
+      |> Repo.get!(topic_id)
+
+    render conn, "show.html", topic: topic
+  end
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
     render conn, "new.html", changeset: changeset
